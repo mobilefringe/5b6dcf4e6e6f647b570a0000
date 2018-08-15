9f203fece0647e7fac13a6dafe14caaa33de1418
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-08-15 18:03:34 -0400

    East Washington Place 1.1  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -21,7 +21,7 @@
                                     </div>
                                     <div class="col-sm-6" >
                                         <label for="fieldzhtulk" class="accessibility">Last Name</label>
-                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldfihudi" name="cm-f-zhtulk" type="text" placeholder="Last Name">
+                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldzhtulk" name="cm-f-zhtulk" type="text" placeholder="Last Name">
                                     </div>
                                 </div>
                                 <div class="row">
