0d7d97c3a1bdd0c7b2b7a33f8d2590f38ede48ab
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-19 20:42:58 -0400

    East Washington Place 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -194,12 +194,12 @@
                     var store_list = [];
                     var vm = this;
                     _.forEach(this.processedStores, function(value, key) {
-                        // if(!_.includes(value.categories, vm.dineFilter)) {
-                            if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1529516445000/cerritos.png";
-                            }
-                            store_list.push(value);
-                        // }
+                        if (_.includes(value.image_url, 'missing')) {
+                            value.no_store_logo = true;
+                        } else {
+                            value.no_store_logo = false;
+                        }
+                        store_list.push(value);
                     });
                     this.filteredStores = store_list;
                     return store_list
