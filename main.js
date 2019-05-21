62135a9198e4cbe6477103d70fa82de0827fafec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-21 16:12:27 -0400

    East Washington Place 1.0  /main.js

--- main.js
+++ main.js
@@ -178,11 +178,15 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
         methods: {
             loadData: async function() {
                 try{
-                    await this.$store.dispatch('initializeApi', { site: "bramaleacitycentre", version: "v4" });
+                    await this.$store.dispatch('initializeApi', { site: "eastwashington", version: "v4" });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
-                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores")]);
-                    // await Promise.all([this.$store.dispatch("LOAD_META_DATA")]);
+                    let results = await Promise.all([
+                        this.$store.dispatch("INITIALIZE_LOCALE"), 
+                        this.$store.dispatch("getData", "hours"), 
+                        this.$store.dispatch("getData", "stores")
+                    ]);
+                    await Promise.all([this.$store.dispatch("LOAD_META_DATA")]);
                     return results;
                 } catch (e) {
                     console.log("Error loading data: " + e.message);    
