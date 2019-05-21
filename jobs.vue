b3cd43e4aa8c61c73ddf9e0e49562c3fd5d3be27
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-21 13:11:34 -0400

    East Washington Place 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -11,56 +11,50 @@
 		</div>
 		<div class="site_container page_content">
 			<div id="events_container" v-if="promotions.length > 0">
-				<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="4">
-					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }">
-						<div class="col-sm-6 col-md-4 event_image_container">
-							<!--<router-link :to="'/jobs/'+ promo.slug" class="event_learn_more">-->
-								<img :src="promo.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + promo.name"/>
-							<!--</router-link>-->
+				
+				<div class="row event_container" v-for="(item, index) in jobs" :class="{ 'last': index === (jobs.length - 1) }">
+					<div class="col-sm-6 col-md-4 event_image_container">
+						<!--<router-link :to="'/jobs/'+ item.slug" class="event_learn_more">-->
+							<img :src="item.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + item.name"/>
+						<!--</router-link>-->
+					</div>
+					<div class="col-sm-6 col-md-8 event_dets_container">
+						<h4 class="event_name caps"  v-if="locale=='en-ca'">{{item.name}}</h4>
+						<h4 class="event_name caps"  v-else>{{item.name_2}}</h4>
+						<div v-if="item.jobable_type == 'Store'">
+						    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{item.store.name}}</h4>
+						    <h4 class="event_store_name caps" v-else>{{item.store.name_2}}</h4>
 						</div>
-						<div class="col-sm-6 col-md-8 event_dets_container">
-							<h4 class="event_name caps"  v-if="locale=='en-ca'">{{promo.name}}</h4>
-							<h4 class="event_name caps"  v-else>{{promo.name_2}}</h4>
-							<div v-if="promo.jobable_type == 'Store'">
-    						    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>
-    						    <h4 class="event_store_name caps" v-else>{{promo.store.name_2}}</h4>
-    						</div>
-							<div class="event_thick_line"></div>
-							<p class="event_dates">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
-							<p class="event_desc"  v-if="locale=='en-ca'">{{promo.description_short}}</p>
-							<p class="event_desc"  v-else>{{promo.description_short_2}}</p>
-							<div class="text-right  col-sm-6" v-if="promo" style="padding:0">
-								<router-link :to="'/jobs/'+ promo.slug" class="event_learn_more pull-left"  :aria="promo.name">
-									{{$t("jobs_page.read_more")}} <i class="fa fa-angle-right" aria-hidden="true"></i>
-								</router-link>
-								<social-sharing :url="shareURL(promo.slug)" :title="promo.name" :description="promo.description" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="BCCstyle" :media="promo.image_url" inline-template >
-									<div class="blog-social-share pull_right">
-										<div class="social_share">
-											<network network="facebook">
-												<i class="fa fa-facebook social_icons" aria-hidden="true"></i>
-											</network>
-											<network network="twitter">
-												<i class="fa fa-twitter social_icons" aria-hidden="true"></i>
-											</network>
-										</div>
+						<div class="event_thick_line"></div>
+						<p class="event_dates">{{item.start_date | moment("MMM D", timezone)}} - {{item.end_date | moment("MMM D", timezone)}}</p>
+						<p class="event_desc"  v-if="locale=='en-ca'">{{item.description_short}}</p>
+						<p class="event_desc"  v-else>{{item.description_short_2}}</p>
+						<div class="text-right  col-sm-6" v-if="item" style="padding:0">
+							<router-link :to="'/jobs/'+ item.slug" class="event_learn_more pull-left"  :aria="item.name">
+								{{$t("jobs_page.read_more")}} <i class="fa fa-angle-right" aria-hidden="true"></i>
+							</router-link>
+							<social-sharing :url="shareURL(item.slug)" :title="item.name" :description="item.description" :quote="_.truncate(item.description, {'length': 99})" twitter-user="BCCstyle" :media="item.image_url" inline-template >
+								<div class="blog-social-share pull_right">
+									<div class="social_share">
+										<network network="facebook">
+											<i class="fa fa-facebook social_icons" aria-hidden="true"></i>
+										</network>
+										<network network="twitter">
+											<i class="fa fa-twitter social_icons" aria-hidden="true"></i>
+										</network>
 									</div>
-								</social-sharing>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<hr>
+								</div>
+							</social-sharing>
 						</div>
 					</div>
-				</paginate>
+					<div class="col-sm-12">
+						<hr>
+					</div>
+				</div>
 			</div>
 			<div id="no_events" class="row" v-else>
 				<div class="col-md-12">
 					<p>{{$t("jobs_page.no_job_message")}}</p>
-				</div>
-			</div>
-			<div class="row margin-60">
-				<div class="col-md-12">
-					<paginate-links for="promos" :async="true" :limit="5" :show-step-links="true"></paginate-links>
 				</div>
 			</div>
 		</div>
@@ -78,8 +72,6 @@
                     filteredPromos:[],
                     dataloaded: false,
                     promoBanner: null,
-                    paginate: ['promos'],
-                    promos : null
                 }
             },
             created() {
@@ -90,7 +82,7 @@
                     if(temp_repo) {
                         this.promoBanner = temp_repo.images[0];
                     }
-                    this.promos = this.promotions;
+
                 });
             },
             computed: {
@@ -100,7 +92,7 @@
                     'processedJobs',
                     'findRepoByName',
                 ]),
-                promotions() {
+                jobs() {
                     var vm = this;
                     var temp_promo = [];
                     var temp_job = [];
