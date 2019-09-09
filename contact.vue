9117579f47da42b3eaf8e33669bc111fccd76694
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-09 12:22:07 -0400

    East Washington Place 1.1  /contact.vue

--- contact.vue
+++ contact.vue
@@ -19,7 +19,7 @@
                             <div v-if="main" class="margin_60" v-html="main.body"></div>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row" id="contact_us_container">
                         <div class="col-md-4">
                             <div v-if="contactInfo" class="margin_60 padding_60" v-html="contactInfo.body"></div>    
                         </div>
