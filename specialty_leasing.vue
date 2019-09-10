db02e45398b2ccf4f8e8011cba0f88285903f058
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-10 12:45:42 -0400

    East Washington Place 1.1  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -92,8 +92,14 @@
                         }
                     }
 
-                    this.main = response[1].data;
-                    this.leasingInfo = response[1].data.subpages[0];
+                    // this.main = response[1].data;
+                    // this.leasingInfo = response[1].data.subpages[0];
+                    if(response && response[1]){
+                        this.main = response[1].data;
+                        if(response[1].data && response[1].data.subpages && response[1].data.subpages[0]){
+                            this.leasingInfo = response[1].data.subpages[0]
+                        }
+                    }
                     this.dataLoaded = true;
                 });
             },
