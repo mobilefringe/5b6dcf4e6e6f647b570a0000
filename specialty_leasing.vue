976d686c1507deb4c53db2a6916d538749d61679
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-10 12:58:04 -0400

    East Washington Place 1.1  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -34,11 +34,6 @@
                         </div>
                     </div>
                 </div>
-                <!--<div class="location_image_container">-->
-                <!--    <div class="location_image" v-if="pageImages" v-for="item in pageImages">-->
-                <!--        <img :src="item.image_url" alt="" />   -->
-                <!--    </div>-->
-                <!--</div>-->
                 <div class="location_image_container">
                     <div class="location_image" v-if="pageImages" v-for="item in pageImages">
                         <img :src="item.image_url" alt="" />   
