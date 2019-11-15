5811a556242d143e81987e496fe1619cd8fb21f1
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-14 20:15:05 -0500

    East Washington Place 1.2  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -40,7 +40,6 @@
             created() {
                 this.updateCurrentPage(this.id);
                 var temp_repo = this.findRepoByName('Pages Banner');
-                console.log(temp_repo)
                     if(temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
